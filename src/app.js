aa14d55db5a4d1823778dc921a6fb47940fe95b5
WillRike <will_rike@hotmail.com>
2021-12-22 10:09:15 -0300

    config scripts package.json

--- src/app.js
+++ src/app.js
@@ -18,7 +18,8 @@ class App {
 
   middlewares() {
     this.server.use(cors());
-
+    this.server.use(express.json());
+    this.server.use(express.urlencoded({ extended: true }));
     this.server.use(express.json());
   }
 
